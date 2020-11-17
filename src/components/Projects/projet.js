5184f576d5e82aff898e79aaa5ebc1fa03320357
Pouiks <virgilejoinville@gmail.com>
2020-11-17 13:25:20 +0100

    css changement #6

--- src/components/Projects/projet.js
+++ src/components/Projects/projet.js
@@ -13,15 +13,14 @@ const Projet = ({
     <a href={link} target="_blank" className="projet-title">{title}</a>
     <div className="card-project">
       <img className="card-project-img" src={image} alt="Projet" />
-      <h3 className="card-projet-h3">{h3}</h3>
-      <h4 className="card-projet-h4">{h4}</h4>
-      <p className="card-projet-desc">{description}</p>
-      <ModalProject link={link} title={title} image={image} h3={h3} h4={h4} description={description} />
+      <div className="card-projet-text">
+        <h3 className="card-projet-h3">{h3}</h3>
+        <h4 className="card-projet-h4">{h4}</h4>
+        <p className="card-projet-desc">{description}</p>
+        <ModalProject link={link} title={title} image={image} h3={h3} h4={h4} description={description} />
+      </div>
     </div>
   </div>
 );
 
 export default Projet;
-
-
-

65671ee45338675e5d33beb07c56a04ce26a543e
Pouiks <virgilejoinville@gmail.com>
2020-11-20 18:20:59 +0100

    css for homepage, about and project is responsive

--- src/components/Projects/projet.js
+++ src/components/Projects/projet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalProject from 'src/components/ModalProject';
+import './projects.scss';
 
 const Projet = ({
   link,
@@ -17,7 +18,7 @@ const Projet = ({
         <h3 className="card-projet-h3">{h3}</h3>
         <h4 className="card-projet-h4">{h4}</h4>
         <p className="card-projet-desc">{description}</p>
-        <ModalProject link={link} title={title} image={image} h3={h3} h4={h4} description={description} />
+          <ModalProject link={link} title={title} image={image} h3={h3} h4={h4} description={description} />
       </div>
     </div>
   </div>
